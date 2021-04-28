cbce34699615987cad4a47ba7e26882b67b23342
jennafritz <jennafritz24@gmail.com>
2021-04-28 18:37:12 -0400

    commit tree af5eb16123f840548b675a21db28f82f1a4e7bef

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,39 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
+  constructor(props) {
     super();
 
-    this.state = {};
+    this.state = {
+      message: "",
+      remainder: props.maxChars
+    };
   }
+
+  showRemainder = event => {
+    let remainder = (this.props.maxChars - event.target.value.length)
+    this.setState({
+      [event.target.name]: event.target.value,
+      remainder
+    })
+  }
+
+
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input 
+          type="text" 
+          name="message" 
+          id="message" 
+          value = {this.state.message}
+          onChange = {event => 
+            this.showRemainder(event)
+          }
+          />
+          <p>You have {this.state.remainder} characters remaining.</p>
       </div>
     );
   }
